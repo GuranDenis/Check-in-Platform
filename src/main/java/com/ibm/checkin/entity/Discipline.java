7bb4130da5167485045b9ef50d84a6b5c98da9cd
DenisGuran <denis.guran00@e-uvt.ro>
2022-01-03 13:59:32 +0200

    Discipline imports

--- src/main/java/com/ibm/checkin/entity/Discipline.java
+++ src/main/java/com/ibm/checkin/entity/Discipline.java
@@ -1,8 +1,5 @@
 package com.ibm.checkin.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.persistence.*;
